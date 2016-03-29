c4fe7a5b27d55a274af0b7c452797d268de1a01a
Oxflick <flick@funkyflick.com>
2016-03-28 21:16:07 -0700

    First apges commit

---
+++ README.md
@@ -0,0 +1,15 @@
+<h1> Twith TV App </h1>
+
+<p> In this project I focused to display the set of streamers from Twitch API by utilizing getJSON and iterating tru objects. </p>
+
+<p> The App shows streamers with indication if any of them broadcasts at the moment. If they do, you can see the additional details about what they are streaming. </p>
+
+<p> User can click on the name and be sent directly to the channel. </p>
+
+<p> User can also see a notification if a streamer has closed their Twitch account (or the account never existed). </p>
+
+<p> This is JavaScript and jQuery project </p>
+
+
+
+
---
+++ tvapp.js
@@ -0,0 +1,104 @@
+
+$(document).ready(function() {
+  // array of stream users we are interested to display
+  var streamers = ["freecodecamp", "storbeck", "terakilobyte", "LiveWithJazza", "habathcx","RobotCaleb","thomasballinger","noobs2ninjas","beohoff", "californithon", "brunofin", "comster404"]	
+  // loop try json calls with each users
+  for (var j=0; j < streamers.length; j++) {
+  	var twUrl = 'https://api.twitch.tv/kraken/channels/' + streamers[j] + '';
+  	$.getJSON(twUrl, {
+
+  	})
+  	.done(function(data) {
+      //console.log(data);
+      var names = [];
+  	  var logos = [];
+  	  var links = [];
+  	  names.push(data.display_name);
+  	  logos.push(data.logo);
+  	  links.push(data.url);
+  	  //console.log(names);
+
+       // append results to the page
+      $('.result').append('<a class="eachresult" href="' + links[0] + '" target="_blank"><p class="name"> <img class="logo thumbnail" src="' + logos[0] + '" alt="logo"><span>' + names[0] + '</span></p></a>');
+
+      // if null or empty add placeholder to logo image
+      $(".logo").each(function() {
+
+       	if($(this).attr("src") == "null" || $(this).attr("src") == '') {
+
+       	  $(this).attr("src", "http://placehold.it/50x50/11d476/fff");
+       	}
+
+      });
+    })
+    // handle the errors for users who does not have account or suspended
+    .fail(function(jqxhr, textStatus, error) {
+         
+         var jsonResponse = [];
+         jsonResponse.push(JSON.parse(jqxhr.responseText));
+
+         for (var z = 0; z < jsonResponse.length; z++) {
+         	
+         	$('.result').append('<a class="eachresult" href="https://www.twitch.tv/" target="_blank"><p class="name"> <img class="logo thumbnail" src="http://placehold.it/50x50/11d476/fff" alt="logo"><span>' + jsonResponse[z].message + '</span></p></a>')
+         }
+    });
+
+  }
+
+  // json call to retrieve if user online or offline
+  var strUrl = 'https://api.twitch.tv/kraken/streams?channel=' + streamers + '';
+  $.getJSON(strUrl, {
+
+  })
+  .done(function(json) {
+	var displayname = [];
+
+    for (var i = 0; i < json.streams.length; i++) {
+    	displayname.push([json.streams[i].channel.display_name, json.streams[i].channel.status]);
+    	//description.push(json.streams[i].channel.status);
+        //console.log(displayname);
+
+    }
+
+    // find out if display name is in the response change the color to green to online color
+    for (var u=0; u < displayname.length; u++) {
+    	$(".name span").each(function( i ) {
+    		if ( $(this).text() == displayname[u][0] ) {
+    			this.style.color = "green";
+    			$(this).append('<p class="description"> About:' + ' ' + displayname[u][1] + '</p>');
+    		}
+    	});
+    }   
+  });
+
+  // listen for click events on status buttons
+  $(".online" ).click(function() {
+    $(".name span").each(function(i) {
+    	if ( this.style.color != "green") {
+    		$(this).parent().hide();
+    	} else {
+    		$(this).parent().show();
+    	}
+
+    })
+  });
+
+  $(".offline" ).click(function() {
+    $(".name span").each(function(i) {
+    	if ( this.style.color == "green") {
+    		$(this).parent().hide();
+    	} else {
+    		$(this).parent().show();
+    	}
+    })
+  });
+
+  $(".allstates" ).click(function() {
+    $(".name span").each(function(i) {
+    	$(this).parent().show();
+    })
+  });
+
+}); 
+
+
